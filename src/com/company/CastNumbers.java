2ae29df083a8f701d6d94195b4825f0ca1ef10f8
Jo Piene <joluca@gmx.net>
2021-12-01 22:57:28 +0100

    Itso bootifull now

--- src/com/company/CastNumbers.java
+++ src/com/company/CastNumbers.java
@@ -3,8 +3,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class CastNumbers {
 
@@ -17,7 +16,7 @@ public class CastNumbers {
         try {
             String line;
             int i = 0;
-            while ((line =br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 array[i] = line;
                 //System.out.println(array[i]);
                 i++;
@@ -29,6 +28,7 @@ public class CastNumbers {
         }
         return array;
     }
+
     public int[] parseToInt(String[] messung) {
 
         int size = 2000;
@@ -37,5 +37,56 @@ public class CastNumbers {
             intArray[i] = Integer.parseInt(messung[i]);
         }
         return intArray;
+    }
+
+    public void IncrDecrOut(int[] messung) {
+        int[] messungInt = messung;
+        int decrease = 0;
+        int increase = 0;
+
+        for (int i = 0; i < messungInt.length - 1; i++) {
+            if (messungInt[i] < messungInt[i + 1]) {
+                increase += 1;
+                continue;
+            } else {
+                decrease += 1;
+            }
+        }
+        System.out.println("Eine Steigerung in den Messdaten war: " + increase + " mal zu verzeichnen.");
+        System.out.println("Und " + decrease + " Abstiege.");
+    }
+
+    public void threeMeasurementSliding (int[] messung) {
+        int[] messungInt = messung;
+        // Stern zwei
+        int incrCounter = 0;
+        int noChangeCounter = 0;
+        int decCounter = 0;
+
+        for (int start = 0; start < messungInt.length - 2; start++) {
+            int dreierSumme = 0;
+            int vergleichsSumme = 0;
+
+            int[] erstenDrei = Arrays.copyOfRange(messungInt, start, start + 3);
+            for (int value : erstenDrei) {
+                dreierSumme += value;
+            }
+
+            int[] naechstenDrei = Arrays.copyOfRange(messungInt, start + 1, start + 4);
+            for (int j : naechstenDrei) {
+                vergleichsSumme += j;
+            }
+
+            if (dreierSumme < vergleichsSumme) {
+                incrCounter += 1;
+            } else if (dreierSumme == vergleichsSumme) {
+                noChangeCounter += 1;
+            } else {
+                decCounter += 1;
+            }
+        }
+        System.out.println("Die Messungen waren: " + incrCounter + " mal höher.\nDie Messungen waren: " + decCounter + " mal niedriger.");
+        System.out.println("Und Messungen waren: " + noChangeCounter + " mal gleich");
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,62 +1,20 @@
 package com.company;
 
 import java.io.*;
-import java.text.*;
-import java.util.*;
 import java.lang.String;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-
         CastNumbers castNumbers = new CastNumbers();
+        System.out.println("oO.-.-.-.-.-.-.-.-<<^>>-.-.-.-.-.-.-.-.-<<^>>-.-.-.-.-.-.-.-.Oo");
         int[] messungInt = castNumbers.parseToInt(castNumbers.readfile());
-        int decrease = 0;
-        int increase = 0;
-
-        for (int i = 0; i < messungInt.length - 1; i++) {
-            if (messungInt[i] < messungInt[i + 1]) {
-                increase += 1;
-                continue;
-            } else {
-                decrease += 1;
-            }
-            // System.out.println(increase);
-            // System.out.println(decrease);
-        }
-        System.out.println("Eine Steigerung in den Messdaten war: " + increase + " mal zu verzeichnen.");
-        System.out.println("Und "+ decrease + " Abstiege.");
-
-        // Stern zwei
-        int incrCounter = 0;
-        int noChangeCounter = 0;
-        int decCounter = 0;
-
-        for (int i = 0; i < messungInt.length - 2; i++) {
-            int dreierSumme = 0;
-            int vergleichsSumme = 0;
-            int start = i;
-            int end = start + 3;
-            int[] erstenDrei = Arrays.copyOfRange(messungInt, start, end);
-
-            for (int k = 0; k < erstenDrei.length; k++) {
-                dreierSumme += erstenDrei[k];
-            }
-            int[] naechstenDrei = Arrays.copyOfRange(messungInt, start + 1, end + 1);
-            for (int j : naechstenDrei) {
-                vergleichsSumme += j;
-            }
-
-            if (dreierSumme < vergleichsSumme) {
-                incrCounter += 1;
-            } else if (dreierSumme == vergleichsSumme) {
-                noChangeCounter += 1;
-            } else {
-                decCounter += 1;
-            }
-        }
-        System.out.println("Die Messungen waren: " + incrCounter + " mal höher.\nDie Messungen waren: " + decCounter + " mal niedriger.");
-        System.out.println("Und Messungen waren: " + noChangeCounter + " mal gleich");
+        castNumbers.IncrDecrOut(messungInt);
+        System.out.println("oO.-.-.<^>.-.-.Oo");
+        System.out.println(" ");
+        System.out.println("oO.-.-.-.-.-.-.-.-<<^>>-.-.-.-.-.-.-.-.Oo");
+        castNumbers.threeMeasurementSliding(messungInt);
+        System.out.println("oO.-.-.-.-.-.-.-.-<<^>>-.-.-.-.-.-.-.-.Oo");
     }
 }
 
